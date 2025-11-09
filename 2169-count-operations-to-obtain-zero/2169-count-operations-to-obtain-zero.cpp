be93732d1509674e301d1fe298a70eb4ed04bd4d
sakshi006 <44032674+sakshi006@users.noreply.github.com>
2025-11-09 20:16:47 +0530

    Time: 0 ms (100%), Space: 7.8 MB (71.74%) - LeetHub

--- 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.cpp
@@ -1,13 +1,6 @@
 class Solution {
 public:
     int countOperations(int num1, int num2) {
-        // int a = min(num1, num2);
-        // int b = max(num1, num2);
-        // if(num1%2 == num2%2){ //either both even or both odd - same parity
-        //     return (b/a + b%a );
-        // } else {
-            
-        // } 
         long sum = 0;
         while(num1!=0 && num2!=0){
             int a = min(num1, num2);
